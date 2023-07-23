cc0e80b30f4517fad48c407df8221bf03c4c96d6
Michael Astrauckas <1589169+mastrauckas@users.noreply.github.com>
2023-07-23 16:03:23 -0400

    Added a global using.

--- src/Maa.Vacations.Dtos/CreateVacationDto.cs
+++ src/Maa.Vacations.Dtos/CreateVacationDto.cs
@@ -1,4 +1,3 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿namespace Maa.Vacations.Dtos;
 
-namespace Maa.Vacations.Dtos;
 public record CreateVacationDto([Required, StringLength(100, MinimumLength = 5)] string Name);
--- src/Maa.Vacations.Dtos/UpdateVacationDto.cs
+++ src/Maa.Vacations.Dtos/UpdateVacationDto.cs
@@ -1,5 +1,3 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace Maa.Vacations.Dtos;
+﻿namespace Maa.Vacations.Dtos;
 
 public record UpdateVacationDto([Required, StringLength(100, MinimumLength = 5)] string Name);
