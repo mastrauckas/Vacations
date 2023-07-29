3b35ccb7eb5a066cd6ac13fcbe004f771448b656
Michael Astrauckas <1589169+mastrauckas@users.noreply.github.com>
2023-07-29 11:25:39 -0400

    Save changes.

---
+++ src/Maa.Vacations.Tests/VacationRouteTest.cs
@@ -0,0 +1,13 @@
+﻿namespace Maa.Vacations.Tests;
+
+public class VacationRouteTest
+{
+    [Theory, VacationIntegrationAutoData]
+    public void MakeSureVacationIsSaved2Test(HttpClient client)
+    {
+        var i = 0;
+        i++;
+
+        Assert.NotNull(client);
+    }
+}
