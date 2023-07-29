b7117a27e8dbc986c2d3a2027ec9d091769c04ad
Michael Astrauckas <1589169+mastrauckas@users.noreply.github.com>
2023-07-29 11:25:14 -0400

    New broken test.

---
+++ src/Maa.Vacations.Tests/Attributes/VacationIntegrationAutoData.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace Maa.Vacations.Tests;
+
+public class VacationIntegrationAutoData : AutoDataAttribute
+{
+    public VacationIntegrationAutoData() : base(
+        () =>
+        {
+            using var application = new WebApplicationFactory<Program>();
+            using var client = application.CreateClient();
+            var fixture = new Fixture();
+            var configuration = new MapperConfiguration(cfg => cfg.AddProfile<ProfileVacation>());
+            var mapper = configuration.CreateMapper();
+            fixture.Inject(mapper);
+            fixture.Inject(client);
+            fixture.Customize(new AutoMoqCustomization());
+            return fixture;
+        })
+    {
+    }
+}
+
