5a4c34f241d760f1adf7c31ff4abe6490deb363c
Michael Astrauckas <1589169+mastrauckas@users.noreply.github.com>
2023-08-05 10:25:47 -0400

    Makes client a class member.

--- src/Maa.Vacations.Tests/VacationRouteTest.cs
+++ src/Maa.Vacations.Tests/VacationRouteTest.cs
@@ -31,7 +31,7 @@ public class VacationRouteTest : WebApplicationFactory<Program>
     [Fact]
     public async Task Make_Http_Get_Request_Test()
     {
-        await MakeHttpRequest(_client);
+        await MakeHttpRequest();
     }
 
     [Theory]
@@ -45,7 +45,7 @@ public class VacationRouteTest : WebApplicationFactory<Program>
     public async Task Create_A_Vacation_Test(string vactionName, HttpStatusCode statusCode)
     {
         CreateVacationDto createVacationDto = new(vactionName);
-        var vacationCreatedDto = await MakeHttpRequest<VacationCreatedDto>(_client, expectedHttpStatusCode: statusCode, method: HttpMethod.Post, body: createVacationDto);
+        var vacationCreatedDto = await MakeHttpRequest<VacationCreatedDto>(expectedHttpStatusCode: statusCode, method: HttpMethod.Post, body: createVacationDto);
         if (statusCode == HttpStatusCode.Created)
         {
             Assert.Equal(createVacationDto.Name, vacationCreatedDto.Name);
@@ -53,9 +53,8 @@ public class VacationRouteTest : WebApplicationFactory<Program>
         }
     }
 
-    private async Task MakeHttpRequest(HttpClient client, HttpStatusCode expectedHttpStatusCode = HttpStatusCode.OK, HttpMethod method = null, object body = null, string contentType = "application/json")
+    private async Task MakeHttpRequest(HttpStatusCode expectedHttpStatusCode = HttpStatusCode.OK, HttpMethod method = null, object body = null, string contentType = "application/json")
     {
-        Assert.NotNull(client);
         var requestUri = $"http://localhost/vacations";
         using var request = new HttpRequestMessage(method ?? HttpMethod.Get, requestUri);
         if (body is not null)
@@ -64,13 +63,12 @@ public class VacationRouteTest : WebApplicationFactory<Program>
             request.Content = new StringContent(payload, Encoding.UTF8, contentType);
         }
 
-        var response = await client.SendAsync(request);
+        var response = await _client.SendAsync(request);
         Assert.Equal(response.StatusCode, expectedHttpStatusCode);
     }
 
-    private async Task<TResponseObject> MakeHttpRequest<TResponseObject>(HttpClient client, HttpStatusCode expectedHttpStatusCode = HttpStatusCode.OK, HttpMethod method = null, object body = null, string contentType = "application/json")
+    private async Task<TResponseObject> MakeHttpRequest<TResponseObject>(HttpStatusCode expectedHttpStatusCode = HttpStatusCode.OK, HttpMethod method = null, object body = null, string contentType = "application/json")
     {
-        Assert.NotNull(client);
         var requestUri = $"http://localhost/vacations";
         using var request = new HttpRequestMessage(method ?? HttpMethod.Get, requestUri);
         if (body is not null)
@@ -79,7 +77,7 @@ public class VacationRouteTest : WebApplicationFactory<Program>
             request.Content = new StringContent(payload, Encoding.UTF8, contentType);
         }
 
-        var response = await client.SendAsync(request);
+        var response = await _client.SendAsync(request);
         Assert.Equal(response.StatusCode, expectedHttpStatusCode);
 
         var content = await response.Content.ReadAsStringAsync();
