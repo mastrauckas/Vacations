35644ce1a364ada8c350d32b8f5eae412aa6283d
Michael Astrauckas <1589169+mastrauckas@users.noreply.github.com>
2023-08-05 10:51:30 -0400

    Cleaning up.

--- src/Maa.Vacations.WebApi/Services/ConfigureServicesExtension.cs
+++ src/Maa.Vacations.WebApi/Services/ConfigureServicesExtension.cs
@@ -4,15 +4,10 @@ public static class ConfigureServicesExtension
 {
     public static void ConfigureService<TDbContext>(this IServiceCollection services, string? connectionString) where TDbContext : DbContext
     {
-        if (connectionString is null)
-        {
-            throw new ArgumentNullException(nameof(connectionString));
-        }
-        // services.AddDbContext<VactionsContext>(c => c.UseSqlite(connectionString));
+        ArgumentException.ThrowIfNullOrEmpty(connectionString);
         services.AddDbContext<TDbContext>(c => c.UseSqlite(connectionString));
         services.AddTransient<IVacationService, VacationService>();
         services.AddTransient<IVacationRepository, VacationRepository>();
-
         services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
     }
 }
