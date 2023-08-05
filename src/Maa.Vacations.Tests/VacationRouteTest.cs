81f20bebb2576a4a702939773b48a67bb0bf9674
Michael Astrauckas <1589169+mastrauckas@users.noreply.github.com>
2023-08-05 10:45:53 -0400

    Some refactoring.

--- src/Maa.Vacations.Tests/VacationRouteTest.cs
+++ src/Maa.Vacations.Tests/VacationRouteTest.cs
@@ -9,21 +9,21 @@ public class VacationRouteTest : BaseIntegrationTest<Program>
     }
 
     [Theory]
-    [VacationIntegrationInlineAutoData("Test Vacation", HttpStatusCode.Created)]
-    [VacationIntegrationInlineAutoData(null, HttpStatusCode.BadRequest)]
-    [VacationIntegrationInlineAutoData("1", HttpStatusCode.BadRequest)]
-    [VacationIntegrationInlineAutoData("12", HttpStatusCode.BadRequest)]
-    [VacationIntegrationInlineAutoData("123", HttpStatusCode.BadRequest)]
-    [VacationIntegrationInlineAutoData("1234", HttpStatusCode.BadRequest)]
-    [VacationIntegrationInlineAutoData("12345", HttpStatusCode.Created)]
-    public async Task Create_A_Vacation_Test(string vactionNameExpected, HttpStatusCode statusCodeExpected)
+    [VacationIntegrationInlineAutoData("Test Vacation", 1, HttpStatusCode.Created)]
+    [VacationIntegrationInlineAutoData(null, null, HttpStatusCode.BadRequest)]
+    [VacationIntegrationInlineAutoData("1", null, HttpStatusCode.BadRequest)]
+    [VacationIntegrationInlineAutoData("12", null, HttpStatusCode.BadRequest)]
+    [VacationIntegrationInlineAutoData("123", null, HttpStatusCode.BadRequest)]
+    [VacationIntegrationInlineAutoData("1234", null, HttpStatusCode.BadRequest)]
+    [VacationIntegrationInlineAutoData("12345", 1, HttpStatusCode.Created)]
+    public async Task Create_A_Vacation_Test(string vactionNameExpected, int? expectedId, HttpStatusCode statusCodeExpected)
     {
         CreateVacationDto createVacationDto = new(vactionNameExpected);
         var vacationCreatedDto = await MakeHttpRequest<VacationCreatedDto>(expectedHttpStatusCode: statusCodeExpected, method: HttpMethod.Post, body: createVacationDto);
         if (statusCodeExpected == HttpStatusCode.Created)
         {
-            Assert.Equal(createVacationDto.Name, vacationCreatedDto.Name);
-            Assert.Equal(1, vacationCreatedDto.Id);
+            Assert.Equal(vactionNameExpected, vacationCreatedDto.Name);
+            Assert.Equal(expectedId, vacationCreatedDto.Id);
         }
     }
 }
