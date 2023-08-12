fdc4a0b654ab98e3948aa281cc2ec9a89c693745
Michael Astrauckas <1589169+mastrauckas@users.noreply.github.com>
2023-08-12 12:31:39 -0400

    Broken test and refactor.

--- src/Maa.Vacations.WebApi/GlobalUsings.cs
+++ src/Maa.Vacations.WebApi/GlobalUsings.cs
@@ -1,7 +1,7 @@
-global using Maa.Vacations.Services;
+global using Maa.Vacations.Dtos;
 global using Maa.Vacations.Entities;
 global using Maa.Vacations.Repositories;
-global using Maa.Vacations.Dtos;
+global using Maa.Vacations.Services;
 global using Maa.Vacations.WebApi.HttpRouting;
 global using Maa.Vacations.WebApi.Services;
 global using Microsoft.EntityFrameworkCore;
