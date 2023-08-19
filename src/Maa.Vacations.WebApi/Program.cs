2aebc95b516de11524483a331600b9a6b5d6015e
Michael Astrauckas <1589169+mastrauckas@users.noreply.github.com>
2023-08-19 13:46:20 -0400

    Adding migrations on startup.

--- src/Maa.Vacations.WebApi/Program.cs
+++ src/Maa.Vacations.WebApi/Program.cs
@@ -5,8 +5,10 @@ builder.Services.ConfigureService<VacationsContext>(connectionString);
 
 var app = builder.Build();
 
+await app.Services.RunMigrations<VacationsContext>();
 app.UseHttpsRedirection();
 app.ConfigureApi();
 app.Run();
+
 
 public partial class Program { }
---
+++ src/Maa.Vacations.WebApi/Services/MigrationsExtension.cs
@@ -0,0 +1,13 @@
+﻿namespace Maa.Vacations.WebApi.Services;
+
+public static class MigrationsExtension
+{
+    public static async Task RunMigrations<TDbContext>(this IServiceProvider serviceProvider) where TDbContext : DbContext
+    {
+        using var scope = serviceProvider.CreateScope();
+        ArgumentNullException.ThrowIfNull(scope);
+        ArgumentNullException.ThrowIfNull(scope.ServiceProvider);
+        var context = scope.ServiceProvider.GetRequiredService<TDbContext>();
+        await context.Database.MigrateAsync();
+    }
+}
