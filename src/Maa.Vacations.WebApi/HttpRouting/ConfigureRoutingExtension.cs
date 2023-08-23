d91a9ec9952955aa176109f8c6dd48141e91fe99
Michael Astrauckas <1589169+mastrauckas@users.noreply.github.com>
2023-08-22 21:40:15 -0400

    A group of changes.  (#10)

--- src/Maa.Vacations.WebApi/HttpRouting/ConfigureRoutingExtension.cs
+++ src/Maa.Vacations.WebApi/HttpRouting/ConfigureRoutingExtension.cs
@@ -12,34 +12,37 @@ public static class ConfigureRoutingExtension
 
     private static void ConfigureRoutes(WebApplication app)
     {
-        app.MapGet("/", async Task<Ok<IEnumerable<CurrentVacationDto>>> (IVacationService routingService) =>
+        var root = app.MapGroup("/");
+
+
+        root.MapGet("", async Task<Ok<IEnumerable<CurrentVacationDto>>> (IVacationService routingService) =>
         {
             var routes = await routingService.GetAllVacationsAsync();
             return TypedResults.Ok(routes);
         });
 
-        app.MapGet("/{id:int}", async Task<Results<NotFound, Ok<CurrentVacationDto>>> (IVacationService routingService, int id) =>
+        root.MapGet("/{id:int}", async Task<Results<NotFound, Ok<CurrentVacationDto>>> (IVacationService routingService, int id) =>
         {
             var route = await routingService.GetVacationByIdAsync(id);
             return route is null ? TypedResults.NotFound() : TypedResults.Ok(route);
         })
         .WithName("GetVacation");
 
-        app.MapPost("/", async Task<CreatedAtRoute<VacationCreatedDto>> (IVacationService routingService, CreateVacationDto route) =>
+        root.MapPost("/", async Task<CreatedAtRoute<VacationCreatedDto>> (IVacationService routingService, CreateVacationDto route) =>
         {
             var createdRoute = await routingService.AddVacationAsync(route);
             return TypedResults.CreatedAtRoute(createdRoute, "GetVacation", new { id = createdRoute.Id });
         })
         .AddEndpointFilter<ValidateAnnotationsFilter>();
 
-        app.MapPut("/vacations/{id:int}", async Task<Results<NotFound, Ok>> (IVacationService routingService, int id, UpdateVacationDto route) =>
+        root.MapPut("/{id:int}", async Task<Results<NotFound, Ok>> (IVacationService routingService, int id, UpdateVacationDto route) =>
         {
             var routeUpdatedDto = await routingService.UpdateVacationAsync(id, route);
             return routeUpdatedDto is null ? TypedResults.NotFound() : TypedResults.Ok();
         })
         .AddEndpointFilter<ValidateAnnotationsFilter>();
 
-        app.MapDelete("/{id:int}", async Task<Results<NotFound, NoContent>> (IVacationService routingService, int id) =>
+        root.MapDelete("/{id:int}", async Task<Results<NotFound, NoContent>> (IVacationService routingService, int id) =>
         {
             var deletedRouteDto = await routingService.DeleteVacationAsync(id);
             return deletedRouteDto is null ? TypedResults.NotFound() : TypedResults.NoContent();
